3d3293b6b2d98f108d632f40544cfd1b9fbca76a
Tomek Wałkuski <ja@jestem.tw>
2014-11-24 16:46:18 +0100

    Rewrite

--- worklog.go
+++ worklog.go
@@ -7,33 +7,40 @@ import (
 	"time"
 )
 
-const hoursMinutesFormat = "15:04"
+func argsToTimes(args []string) (time.Time, time.Time) {
+	const hoursMinutesFormat = "15:04"
+
+	if len(args) != 3 {
+		log.Fatalln("Usage: worklog start_time end_time")
+	}
+
+	startTime, err := time.Parse(hoursMinutesFormat, args[1])
+	if err != nil {
+		log.Fatalln("Invalid startTime time")
+	}
+
+	endTime, err := time.Parse(hoursMinutesFormat, args[2])
+	if err != nil {
+		log.Fatalln("Invalid endTime time")
+	}
+
+	if endTime.Before(startTime) {
+		log.Fatalln("endTime has to be after startTime")
+	}
+
+	return startTime, endTime
+}
+
+func duration(startTime, endTime time.Time) time.Duration {
+	return endTime.Sub(startTime)
+}
 
 func init() {
 	log.SetFlags(0)
 }
 
 func main() {
-	if len(os.Args) != 3 {
-		log.Println("Usage: worklog start_date end_date")
-		return
-	}
+	startTime, endTime := argsToTimes(os.Args)
 
-	timeFrom, err := time.Parse(hoursMinutesFormat, os.Args[1])
-
-	if err != nil {
-		log.Fatalln("Invalid timeFrom time")
-	}
-
-	timeTo, err := time.Parse(hoursMinutesFormat, os.Args[2])
-
-	if err != nil {
-		log.Fatalln("Invalid timeTo time")
-	}
-
-	if timeTo.Before(timeFrom) {
-		log.Fatalln("timeTo has to be after timeFrom")
-	}
-
-	fmt.Println(timeTo.Sub(timeFrom))
+	fmt.Println(duration(startTime, endTime))
 }
